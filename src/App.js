60a0dbe30dc4871a9e69370edcb0c31a236367e3
javieravidal <126521755+javieravidal@users.noreply.github.com>
2023-06-08 06:57:25 -0400

    Changed the default city

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Fukuoka" />
         </div>
         <footer className="footer">
           This project was coded by{" "}
